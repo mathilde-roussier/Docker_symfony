a1485735af37d5280a6529603dbe66bff1798bbe
mathilde.roussier <mathilde.roussier@laplateforme.io>
2021-06-24 15:20:29 +0200

    Modification docker-compose, initialisation entité, ajout info read me

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Docker_symfony
+
+Test technique 
+
+## Environnement de développement
+
+### Pré-requis
+
+* PHP 7
+* Composer
+* Symfony 4 ou 5
+* Docker
+* Docker-compose
+
+### Lancer l'environnement de développement
+
+```bash
+docker-compose up -d
+```
---
+++ project/src/Entity/FluxRss.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FluxRssRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FluxRssRepository::class)
+ */
+class FluxRss
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_source;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom_source;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $url_source;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $commentaires;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdSource(): ?int
+    {
+        return $this->id_source;
+    }
+
+    public function setIdSource(int $id_source): self
+    {
+        $this->id_source = $id_source;
+
+        return $this;
+    }
+
+    public function getNomSource(): ?string
+    {
+        return $this->nom_source;
+    }
+
+    public function setNomSource(string $nom_source): self
+    {
+        $this->nom_source = $nom_source;
+
+        return $this;
+    }
+
+    public function getUrlSource(): ?string
+    {
+        return $this->url_source;
+    }
+
+    public function setUrlSource(string $url_source): self
+    {
+        $this->url_source = $url_source;
+
+        return $this;
+    }
+
+    public function getCommentaires(): ?string
+    {
+        return $this->commentaires;
+    }
+
+    public function setCommentaires(?string $commentaires): self
+    {
+        $this->commentaires = $commentaires;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
